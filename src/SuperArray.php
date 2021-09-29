ca7066e82bbff055e729d490d624279452d2881f
Fize <chenfengzhan@qq.com>
2021-09-29 15:20:52 +0800

    新增方法 SuperArray::toLevel1()。

--- src/SuperArray.php
+++ src/SuperArray.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace fize\super;
 
 use ArrayObject;
@@ -8,4 +9,27 @@ use ArrayObject;
  */
 class SuperArray extends ArrayObject
 {
+
+    /**
+     * 多维数组转一维数组，键名使用下划线合并
+     * @param array  $array  原数组
+     * @param string $prefix 键名前缀
+     * @return array
+     */
+    public static function toLevel1(array $array, string $prefix = ''): array
+    {
+        $lv1array = [];
+        foreach ($array as $key => $value) {
+            if ($prefix) {
+                $key = $prefix . '_' . $key;
+            }
+            if (is_array($value)) {
+                $lv1array = array_merge($lv1array, self::toLevel1($value, $key));
+            } else {
+                $lv1array[$key] = $value;
+            }
+        }
+        return $lv1array;
+    }
+
 }
