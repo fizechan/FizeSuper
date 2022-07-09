407fec6d026691c025a6bae285bde90f54371014
Fize <chenfengzhan@qq.com>
2022-07-10 03:52:06 +0800

    SuperArray新增方法并进行单元测试。

--- src/SuperArray.php
+++ src/SuperArray.php
@@ -24,14 +24,14 @@ class SuperArray
 
     /**
      * 将数组中的所有键名修改为全大写或小写
-     * @param int  $case
-     * @param bool $change
+     * @param int  $case    0-小写；1-大写；
+     * @param bool $inplace 是否改变原数组
      * @return array
      */
-    public function changeKeyCase(int $case = CASE_LOWER, bool $change = true)
+    public function changeKeyCase(int $case = CASE_LOWER, bool $inplace = true)
     {
         $array = array_change_key_case($this->array, $case);
-        if ($change) {
+        if ($inplace) {
             $this->array = $array;
         }
         return $array;
@@ -39,24 +39,62 @@ class SuperArray
 
     /**
      * 多维数组转一维数组，键名使用下划线合并
-     * @param array  $array  原数组
-     * @param string $prefix 键名前缀
+     * @param string $merger             合并符
+     * @param bool   $keep_numeric_array 是否保持数值数组
+     * @param bool   $inplace            是否改变原数组
      * @return array
      */
-    public static function toLevel1(array $array, string $prefix = ''): array
+    public function toLevel1(string $merger = '_', bool $keep_numeric_array = true, bool $inplace = true): array
+    {
+        $array = self::toLevel1Array($this->array, $merger, $keep_numeric_array);
+        if ($inplace) {
+            $this->array = $array;
+        }
+        return $array;
+    }
+
+    /**
+     * 多维数组转一维数组，键名使用【合并符】$merger进行合并
+     * @param array  $array              多维数组
+     * @param string $merger             合并符
+     * @param string $prefix             前缀
+     * @param bool   $keep_numeric_array 是否保持数值数组
+     * @return array
+     */
+    protected static function toLevel1Array(array $array, string $merger = '_', bool $keep_numeric_array = true, string $prefix = ''): array
     {
         $lv1array = [];
         foreach ($array as $key => $value) {
             if ($prefix) {
-                $key = $prefix . '_' . $key;
+                $key = $prefix . $merger . $key;
             }
             if (is_array($value)) {
-                $lv1array = array_merge($lv1array, self::toLevel1($value, $key));
+                if (self::isNumericArray($value) && $keep_numeric_array) {
+                    $lv1array[$key] = $value;
+                } else {
+                    $lv1array = array_merge($lv1array, self::toLevel1Array($value, $merger, $keep_numeric_array, $key));
+                }
             } else {
                 $lv1array[$key] = $value;
             }
         }
         return $lv1array;
+    }
+
+    /**
+     * 判断数组是不是普通数值数组
+     * @param array $array 数组
+     * @return bool
+     */
+    public static function isNumericArray(array $array)
+    {
+        $index = 0;
+        foreach (array_keys($array) as $key) {
+            if ($index++ !== $key) {
+                return false;
+            }
+        }
+        return true;
     }
 
 }
--- tests/TestSuperArray.php
+++ tests/TestSuperArray.php
@@ -2,10 +2,51 @@
 
 namespace Tests;
 
-use fize\super\SuperArray;
+use Fize\Super\SuperArray;
 use PHPUnit\Framework\TestCase;
 
 class TestSuperArray extends TestCase
 {
 
+    public function testChangeKeyCase()
+    {
+        $array = [
+            'key1' => '哈哈哈，不错哦',
+            'KeY2' => '中都是什么鬼！'
+        ];
+        $spa = new SuperArray($array);
+        $array1 = $spa->changeKeyCase();
+        var_dump($array1);
+        self::assertNotEquals($array, $array1);
+        $array2 = $spa->changeKeyCase(1);
+        var_dump($array2);
+        self::assertNotEquals($array2, $array1);
+    }
+
+    public function testToLevel1()
+    {
+        $array = [
+            'A1' => '这是层级1',
+            'A2' => [
+                'B1' => '这是层级2',
+                'B2' => ['我是个无key数组1！', '我是个无key数组2！'],
+                'B3' => [
+                    'C1' => '厉害了3！',
+                    'C2' => '我还能说啥？',
+                    'C3' => [
+                        'D1' => '单个字段问题也解决了！'
+                    ]
+                ]
+            ]
+        ];
+        $spa = new SuperArray($array);
+        $alv1 = $spa->toLevel1('_', true, false);
+        var_dump($alv1);
+        self::assertNotEquals($alv1, $array);
+        $alv2 = $spa->toLevel1('_', false, false);
+        var_dump($alv2);
+        self::assertNotEquals($alv1, $alv2);
+        $alv3 = $spa->toLevel1();
+        self::assertEquals($alv1, $alv3);
+    }
 }
