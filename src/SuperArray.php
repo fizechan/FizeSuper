42c7501c68cdfee4a08bde4c172c9accefc2c5b4
Fize <chenfengzhan@qq.com>
2022-08-04 13:38:40 +0800

    新增一些Array方法。

--- src/SuperArray.php
+++ src/SuperArray.php
@@ -11,6 +11,32 @@ class SuperArray extends ArrayObject
 {
 
     /**
+     * 将数组中的所有键名修改为全大写或小写
+     * @param int  $case     0-小写；1-大写；
+     * @param bool $exchange 是否变换原数组
+     * @return array
+     */
+    public function changeKeyCase(int $case = CASE_LOWER, bool $exchange = true): array
+    {
+        $array = array_change_key_case($this->getArrayCopy(), $case);
+        if ($exchange) {
+            $this->exchangeArray($array);
+        }
+        return $array;
+    }
+
+    /**
+     * 将数组分割成多个
+     * @param int  $size          每个数组的单元数目
+     * @param bool $preserve_keys 设为 true，可以使 PHP 保留输入数组中原来的键名。
+     * @return array
+     */
+    public function chunk(int $size, bool $preserve_keys = false): array
+    {
+        return array_chunk($this->getArrayCopy(), $size, $preserve_keys);
+    }
+
+    /**
      * 多维数组转一维数组，键名使用下划线合并
      * @param array  $array  原数组
      * @param string $prefix 键名前缀
